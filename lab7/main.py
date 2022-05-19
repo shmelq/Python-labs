c3cdf5cc523c899b8778297288cc4ac8b4c42e0e
Daniil Evstigneev <99168943+shmelq@users.noreply.github.com>
2022-05-19 19:36:24 +0300

    lab7 module

---
+++ lab7/main.py
@@ -0,0 +1,5 @@
+from calculator import Calc
+
+calculator = Calc() # создание экземпляра класса Calc()
+result = calculator.calculate() # обращение к функции calculate внутри экземпляра calculator класса Calc()
+print('Ответ: ', result)
